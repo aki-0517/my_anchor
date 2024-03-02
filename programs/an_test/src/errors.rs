2ba3fe754c5f330b8a0de1ded05aef82475107bd
aki-0517 <akki.pp.777@gmail.com>
2024-03-02 23:23:58 +0900

    set tic-tac-toe

---
+++ programs/an_test/src/errors.rs
@@ -0,0 +1,7 @@
+use anchor_lang::prelude::*;
+use instructions::*;
+use state::game::Tile;
+
+pub mod errors;
+pub mod instructions;
+pub mod state;
--- programs/an_test/src/lib.rs
+++ programs/an_test/src/lib.rs
@@ -1,4 +1,10 @@
 use anchor_lang::prelude::*;
+use instructions::*;
+use state::game::Tile;
+
+pub mod errors;
+pub mod instructions;
+pub mod state;
 
 declare_id!("BuvS4T7L1kFGRiQXiKD76khfLUxq9EqyeX3nH3CEYPPK");
 
@@ -6,10 +12,11 @@ declare_id!("BuvS4T7L1kFGRiQXiKD76khfLUxq9EqyeX3nH3CEYPPK");
 pub mod an_test {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn setup_game(ctx: Context<SetupGame>, player_two: Pubkey) -> Result<()> {
+        instructions::setup_game::setup_game(ctx, player_two)
+    }
+
+    pub fn play(ctx: Context<Play>, tile: Tile) -> Result<()> {
+        instructions::play::play(ctx, tile)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
